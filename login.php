4b5dce2d19c688e50d822537cf7db7cf9b29a707
orcana <40320018+orcana@users.noreply.github.com>
2018-06-18 14:02:32 -0400

    added headers to login
    added header information to the login page

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+<title>Abrille's Cuisine</title>
+    
+	<!-- external style referencing file location-->
+<link rel="stylesheet" href="CSS\style.css" type="text/css"/>
+    <div id ="logo">
+        <img src="logo.jpg" alt = "logo" width ="50px" height="48px" usemap="logo"></img>
+        <map name ="logo">
+        <area shape="rect" coords="0,0,150,150" href="index.php" alt="Logo">
+        </map>
+    </div>
+    <h1>Login</h1>
+    </head>
+    <div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="login.php">Login</a>
+  <a href="menu.php">Menu</a>
+  <a href="seating.php">Seatings</a>
+  <a href="contactus.php">Contact Us</a>
+  <a href="aboutus.php">About Us</a>
+</div>
+
+    
+	
+	
+	
