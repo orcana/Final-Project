6524629a0ce0a4c74849f1f42232de977d1504ab
orcana <40320018+orcana@users.noreply.github.com>
2018-06-18 15:58:36 -0400

    Seating page added
    Page which would allow users to view the seats which would be available to reserve for the restaurant

---
+++ seating.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Abrielle's Dining</title>
+    <div id ="logo">
+        <img src="logo.jpg" alt = "logo" width ="50px" height="48px" usemap="logo"></img>
+        <map name ="logo">
+        <area shape="rect" coords="0,0,150,150" href="index.php" alt="Logo">
+        </map>
+    </div>
+    <h1>About Us</h1>
+<!-- external style referencing file location-->
+<link rel="stylesheet" href="CSS\style.css" type="text/css"/>
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="login.php">Login</a>
+  <a href="menu.php">Menu</a>
+  <a href="seating.php">Seatings</a>
+  <a href="contactus.php">Contact Us</a>
+  <a href="aboutus.php">About Us</a>
+</div>
+</head>
+<body>
+    <table bordercolor="white" rowcolor="white">
+    <tr>
+    <th colspan="2"><h3>Available Seats</h3></th>    
+    </tr>
+        
+    <tr>
+        <th><h2>Table</h2></th>
+        <th><h2>Description</h2></th>
+        
+    </tr>
+    
+    </table>
+    <br>
+    <br>
+  <p align = "center">To make a reservation,<a href="reserveseat.php"><FONT color="blue"> Click here.</FONT></a>
+<div id ="footer">© 2015-2018, Abrielle's Cuisine</div>
+</body>
+</html>
