93f274de72ec17a5faa6bc87ec17f9d3de7aacbe
orcana <40320018+orcana@users.noreply.github.com>
2018-06-18 15:35:22 -0400

    added about us  and contact us page.s
    These pages would hold general information about the website

---
+++ aboutus.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Abrielle's Dining</title>
+    <div id ="logo">
+        <img src="logo.jpg" alt = "logo" width ="50px" height="48px" usemap="logo"></img>
+        <map name ="logo">
+        <area shape="rect" coords="0,0,150,150" href="index.php" alt="Logo">
+        </map>
+    </div>
+    <h1>About Us</h1>
+<!-- external style referencing file location-->
+<link rel="stylesheet" href="CSS\style.css" type="text/css"/>
+<div class="topnav">
+  <a class="active" href="indexe.php">Home</a>
+  <a href="login.php">Login</a>
+  <a href="menu.php">Menu</a>
+  <a href="seating.php">Seatings</a>
+  <a href="contactus.php">Contact Us</a>
+  <a href="aboutus.php">About Us</a>
+</div>
+</head>
+<body>
+    //Information about us here
+    <br>
+    <br>
+    //Hyperlink for the privacy policy here
+<div id ="footer">© 2015-2018, Abrielle's Cuisine</div>
+</body>
+</html>
