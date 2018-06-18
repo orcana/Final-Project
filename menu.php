d0cdc28499f632b875cbe1869dd1f340a22f39c7
orcana <40320018+orcana@users.noreply.github.com>
2018-06-18 15:57:51 -0400

    Pages added - Menu Privacy policy & reserve seat
    Pages added with base coding to fill in later.

---
+++ menu.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Abrielle's Dining</title>
+    <div id ="logo">
+        <img src="logo.jpg" alt = "logo" width ="50px" height="48px" usemap="logo"></img>
+        <map name ="logo">
+        <area shape="rect" coords="0,0,150,150" href="index.php" alt="Logo">
+        </map>
+    </div>
+    <h1>Itallian Cuisine</h1>
+    <!-- external style referencing file location-->
+	<link rel="stylesheet" href="CSS\style.css" type="text/css"/>
+    <div class="topnav">
+ <a class="active" href="index.php">Home</a>
+  <a href="login.php">Login</a>
+  <a href="menu.php">Menu</a>
+  <a href="seating.php">Seatings</a>
+  <a href="contactus.php">Contact Us</a>
+  <a href="aboutus.php">About Us</a>
+</div>
+</head>
+<body>
+    
+//Table which includes pictures and description of the menu item. Should have at least 12-15 items, seperated by appetisers, main courses, dessert, soups ETC ETC of food type.
+<div id ="footer">© 2015-2018, Abrielle's Cuisine</div>
+</body>
+</html>
