63516ac5653d4d06193571ef28e5de4b4f3bf9f7
orcana <40320018+orcana@users.noreply.github.com>
2018-06-18 13:44:22 -0400

    added index
    added an index page which would be the 'home page'. The index page is styled with the CSS

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Abrille's Cuisine</title>
+   
+    <h1>Abrille's Cuisine</h1>
+	
+	<!-- external style referencing file location-->
+	<link rel="stylesheet" href="CSS\style.css" type="text/css"/>
+	
+
+</head>
+<body>
+    
+<!-- General description for the homepage -->  
+   <br><br> <div id ="for_left">
+    <section style="padding-left: 30%">
+   <p>  Welcome to Abrielle's. Our Restaurant offers fine Itallian dining with a resonable price. Our menu includes various dishes that come from across all of Italy.<br> </p>
+    <p>Visit us today, so you too can have your own 'Little taste of Italy'</p>
+    </section>
+    </div> 
+    <section style="padding-left:20%">
+    <div id="for_right"><img src="pics/ravioli.png" width="500" height="300" alt="ravioli"></img></div></section>
+<div id ="footer">© 2015-2018, Abrielle's Cuisine</div>
+</body>
+</html>
