2e21ddc09da3d9499a4c2deb75324381c33fce7d
orcana <40320018+orcana@users.noreply.github.com>
2018-06-18 14:27:09 -0400

    Added variables
    Variables for theSQL code which would sync to the table in the created database using XAMP for the server to run for the website has been created.

---
+++ insert.php
@@ -0,0 +1,38 @@
+<?php
+
+// test
+
+	$_SESSION['message']= '';
+	$con = mysqli_connect('localhost','root','','project cs');
+
+	if ($_SERVER['REQUEST_METHOD']=='POST') {
+	# code...
+
+			$fname= $con->real_escape_string($_POST['firstname']);
+
+			$lname= $con->real_escape_string($_POST['lastname']);
+
+			$contact=$con->real_escape_string($_POST['contact']);
+
+			$address= $con->real_escape_string($_POST['address']);
+
+			$email= $con->real_escape_string($_POST['email']); 
+
+			$password= $con->real_escape_string($_POST['password']);
+
+
+	
+					$sql ="INSERT INTO registeredusers(`firstname`, `lastname`, `contact`, `address`, `email`, `password`)" 
+					."VALUES ('$fname','$lname','$contact','$address','$email','$password')";
+				
+			//if query is successful, redirect to ../php/homepage.php
+				if ($con->query($sql)===true) {
+					$_SESSION['message']="Registration successful! Added $name to the database";
+					header('Location: index.php');
+			
+				}else{
+					$_SESSION['message'] = "Error!!!";
+				}
+		}
+	
+?>
