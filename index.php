7d58c667abd160ac695ff36db28ef0b5bf3afb87
orcana <40320018+orcana@users.noreply.github.com>
2018-06-18 13:52:21 -0400

    added Logo & Navigation bar
    added the logo and navigation bar to the home page (index)

--- index.php
+++ index.php
@@ -2,13 +2,25 @@
 <html>
 <head>
     <title>Abrille's Cuisine</title>
-   
+    <div id ="logo">
+        <img src="logo.jpg" alt = "logo" width ="50px" height="48px" usemap="logo"></img>
+        <map name ="logo">
+        <area shape="rect" coords="0,0,150,150" href="index.php" alt="Logo">
+        </map>
+    </div>
     <h1>Abrille's Cuisine</h1>
 	
 	<!-- external style referencing file location-->
 	<link rel="stylesheet" href="CSS\style.css" type="text/css"/>
 	
-
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="login.php">Login</a>
+  <a href="menu.php">Menu</a>
+  <a href="seating.php">Seatings</a>
+  <a href="contactus.php">Contact Us</a>
+  <a href="aboutus.php">About Us</a>
+</div>
 </head>
 <body>
     
