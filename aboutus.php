f8b36ebd4ea5c342bd0a11fac0db5524a8bfc0e6
orcana <40320018+orcana@users.noreply.github.com>
2018-06-23 12:23:53 -0400

    Added code
    Code to show the logo and allow it to not scroll

--- aboutus.php
+++ aboutus.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Abrielle's Dining</title>
-    <div id ="logo">
-        <img src="logo.jpg" alt = "logo" width ="50px" height="48px" usemap="logo"></img>
-        <map name ="logo">
-        <area shape="rect" coords="0,0,150,150" href="index.php" alt="Logo">
-        </map>
+    <title>Abrille's Cuisine</title>
+    
+	<!-- external style referencing file location-->
+    <link rel="stylesheet" href="CSS\style.css" type="text/css"/>    
+</head>
+<body>
+    <section>       
+      <div id ="logo">
+        <img src="pics/logo.jpg" alt="logo" width ="50px" height="48px" />
     </div>
-    <h1>About Us</h1>
-<!-- external style referencing file location-->
-<link rel="stylesheet" href="CSS\style.css" type="text/css"/>
+       <h1>Abrille's Cuisine</h1>     
+    
+    </section>  
+
+	
 <div class="topnav">
-  <a class="active" href="indexe.php">Home</a>
+  <a class="active" href="index.php">Home</a>
   <a href="login.php">Login</a>
   <a href="menu.php">Menu</a>
   <a href="seating.php">Seatings</a>
   <a href="contactus.php">Contact Us</a>
   <a href="aboutus.php">About Us</a>
 </div>
-</head>
-<body>
     //Information about us here
     <br>
     <br>
--- contactus.php
+++ contactus.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Abrielle's Dining</title>
-    <div id ="logo">
-        <img src="logo.jpg" alt = "logo" width ="50px" height="48px" usemap="logo"></img>
-        <map name ="logo">
-        <area shape="rect" coords="0,0,150,150" href="index.php" alt="Logo">
-        </map>
+    <title>Abrille's Cuisine</title>
+    
+	<!-- external style referencing file location-->
+    <link rel="stylesheet" href="CSS\style.css" type="text/css"/>    
+</head>
+<body>
+    <section>
+       
+      <div id ="logo">
+        <img src="pics/logo.jpg" alt="logo" width ="50px" height="48px" />
     </div>
-    <h1>Contact Us</h1>
-<!-- external style referencing file location-->
-	<link rel="stylesheet" href="CSS\style.css" type="text/css"/>
- <div class="topnav">
+       <h1>Abrille's Cuisine</h1>
+     
+    
+    </section>
+  
+
+	
+<div class="topnav">
   <a class="active" href="index.php">Home</a>
   <a href="login.php">Login</a>
   <a href="menu.php">Menu</a>
@@ -19,8 +27,6 @@
   <a href="contactus.php">Contact Us</a>
   <a href="aboutus.php">About Us</a>
 </div>
-</head>
-<body>
     //Information to contact us here
 
     <div id ="footer">© 2015-2018, Abrielle's Cuisine</div></body>
