7d307b8882c1319ea6766cdf2320bd20a245eb83
orcana <40320018+orcana@users.noreply.github.com>
2018-07-04 11:34:57 -0400

    Created redirect register page
    page created where it would redirect when a registration is successful. Images would be added which would then redirect either to a login page or a home page.

---
+++ redirectlogin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Abrielle's Dining</title>
+    </head>
+    <!-- external style referencing file location-->
+	<link rel="stylesheet" href="CSS\style.css" type="text/css"/>
+
+<body>
+    <section>
+
+       <h1><a href="index.php"><img src="pics/mainlogo.png" alt="logo" width ="120px" height="120px"/></h1>
+     
+    
+    </section>
+  
+
+	
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="login.php">Login</a>
+  <a href="menu.php">Menu</a>
+  <a href="seating.php">Seatings</a>
+  <a href="contactus.php">Contact Us</a>
+  <a href="aboutus.php">About Us</a>
+</div>
+//Images where the redirect after registering would be
+
+    <div id ="footer">© 2015-2018, Abrielle's Cuisine</div>
+</body>
+</html>
