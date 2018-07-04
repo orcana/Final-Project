c4f63763caea89925725f36fe8ffeaac3a4a19bb
orcana <40320018+orcana@users.noreply.github.com>
2018-07-04 11:40:31 -0400

    Duplicated login
    Login duplicated and the hyperlink for registeration was removed, this in turn will be used after successful registeration as a hyperlink to redirect the user if they so choose.

---
+++ login2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Abrille's Cuisine</title>
+    
+	<!-- external style referencing file location-->
+    <link rel="stylesheet" href="CSS\style.css" type="text/css"/>    
+</head>
+    <body>
+    <section>
+
+       <h1><a href="index.php"><img src="pics/mainlogo.png" alt="logo" width ="120px" height="120px"/></h1>
+     
+    
+    </section>
+  
+
+	
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="login.php">Login</a>
+  <a href="menu.php">Menu</a>
+  <a href="seating.php">Seatings</a>
+  <a href="contactus.php">Contact Us</a>
+  <a href="aboutus.php">About Us</a>
+</div>
+
+
+
+    <br><br>
+<div class= "loginbox">
+    <form action="registration.php" method="post">
+        Email<br>
+        <input type="text" email="" placeholder="e-mail@domain.com"/> <br>
+        Password<br>
+        <input type="password" name="" placeholder="*******"/> <br>
+        <input type="submit" name="Login" value="Login" />
+</form>
+</div> 
+<div id ="footer">© 2015-2018, Abrielle's Cuisine</div>
+</body>
+</html>
+
